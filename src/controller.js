1e7a1d1ca0aa9c803110a914fbbcded1c9f48a3c
killiandb <killiandomingues57@gmail.com>
2023-11-17 16:18:11 -0300

    arquivos iniciais e começo de rota post /tasks

---
+++ src/controller.js
@@ -0,0 +1,38 @@
+import {randomUUID} from 'node: crypto'
+
+const Controller = {
+// - `POST - /tasks`
+    
+//     Deve ser possível criar uma task no banco de dados, enviando os campos `title` e `description` por meio do `body` da requisição.
+    
+//     Ao criar uma task, os campos: `id`, `created_at`, `updated_at` e `completed_at` devem ser preenchidos automaticamente, conforme a orientação das propriedades acima.
+  criaTask: async (req, res) => {
+    const data = req.body
+    return res.render('task criada', { data })
+  }
+}
+
+// - `GET - /tasks`
+    
+//     Deve ser possível listar todas as tasks salvas no banco de dados.
+    
+//     Também deve ser possível realizar uma busca, filtrando as tasks pelo `title` e `description`
+    
+// - `PUT - /tasks/:id`
+    
+//     Deve ser possível atualizar uma task pelo `id`.
+    
+//     No `body` da requisição, deve receber somente o `title` e/ou `description` para serem atualizados.
+    
+//     Se for enviado somente o `title`, significa que o `description` não pode ser atualizado e vice-versa.
+    
+//     Antes de realizar a atualização, deve ser feito uma validação se o `id` pertence a uma task salva no banco de dados.
+    
+// - `DELETE - /tasks/:id`
+    
+//     Deve ser possível remover uma task pelo `id`.
+    
+//     Antes de realizar a remoção, deve ser feito uma validação se o `id` pertence a uma task salva no banco de dados.
+    
+// - `PATCH - /tasks/:id/complete`
+module.exports = Controller;
